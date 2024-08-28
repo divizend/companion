f52171253d7db4e6eb03e1c9b562cc65459c6ae6
Julian Nalenz <julian.nalenz@divizend.com>
2024-08-28 21:30:26 +0200

    refactor: put global config into app.config.js

---
+++ app.config.js
@@ -0,0 +1,61 @@
+export default ({ config }) => {
+  const isProduction = process.env.NODE_ENV === "production";
+
+  return {
+    ...config,
+    name: "Divizend",
+    slug: "Divizend",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "divizend",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#00008f",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.divizend.companion",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#00008f",
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: ["expo-router", "expo-secure-store", "expo-localization"],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: isProduction
+      ? {
+          auth: {
+            url: "https://auth.divizend.com",
+            clientId: "1d277b8c-eafa-49f2-a6b1-fa475ae1572c",
+            tenantId: "0440c851-ab0b-d4ff-062a-34e04b26c680",
+          },
+          api: {
+            url: "https://api.divizend.com",
+            versionCode: "v1",
+          },
+        }
+      : {
+          auth: {
+            url: "http://localhost:9011",
+            clientId: "fa4aca16-0d68-433c-9b7f-24ccb1269d28",
+            tenantId: "9c88929b-785c-5e8f-522b-760f28db724e",
+          },
+          api: {
+            url: "http://localhost:3001",
+            versionCode: "v1",
+          },
+        },
+  };
+};
--- app/common/config.tsx
+++ app/common/config.tsx
@@ -1,15 +1,3 @@
-export const CONFIG = {
-  local: {
-    auth: {
-      url: "http://localhost:9011",
-      clientId: "fa4aca16-0d68-433c-9b7f-24ccb1269d28",
-      tenantId: "9c88929b-785c-5e8f-522b-760f28db724e",
-    },
-    api: {
-      url: "http://localhost:3001",
-      versionCode: "v1",
-    },
-  },
-};
+import Constants from "expo-constants";
 
-export const usedConfig = CONFIG.local;
+export const usedConfig = Constants.expoConfig!.extra;
