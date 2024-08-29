5b3ab84a29c0d0b9afd15d91d582ced0be7aaf18
Julian Nalenz <julian.nalenz@divizend.com>
2024-08-29 11:11:52 +0200

    feat: improve error and loading handling during login

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as WebBrowser from "expo-web-browser";
 import { useAuthRequest, useAutoDiscovery } from "expo-auth-session";
 import { router } from "expo-router";
@@ -13,6 +13,7 @@ WebBrowser.maybeCompleteAuthSession();
 export default function Index() {
   const discovery = useAutoDiscovery(usedConfig.auth.url);
   const [sessionToken, sessionTokenLoading] = useSessionToken();
+  const [handleLoginInProgress, setHandleLoginInProgress] = useState(false);
 
   useEffect(() => {
     if (sessionToken && !sessionTokenLoading) {
@@ -36,25 +37,38 @@ export default function Index() {
   );
 
   const handleLogin = async () => {
+    setHandleLoginInProgress(true);
+
     try {
       const response = await promptAsync();
       if (response?.type !== "success") {
         throw new Error(response?.type);
       }
 
-      const { publicTokenGetterCode } = await (
-        await fetch(
-          `${usedConfig.api.url}/${
-            usedConfig.api.versionCode
-          }/auth/callback?${new URLSearchParams(response.params).toString()}`
-        )
-      ).json();
+      const apiCallbackResp = await fetch(
+        `${usedConfig.api.url}/${
+          usedConfig.api.versionCode
+        }/auth/callback?${new URLSearchParams(response.params).toString()}`
+      );
+      if (!apiCallbackResp.ok) {
+        throw new Error(
+          `Failed to get public token getter code (HTTP ${apiCallbackResp.status})`
+        );
+      }
 
-      const { sessionToken } = await (
-        await fetch(
-          `${usedConfig.api.url}/${usedConfig.api.versionCode}/auth/sessionToken/${publicTokenGetterCode}`
-        )
-      ).json();
+      const { publicTokenGetterCode } = await apiCallbackResp.json();
+
+      const getSessionTokenResp = await fetch(
+        `${usedConfig.api.url}/${usedConfig.api.versionCode}/auth/sessionToken/${publicTokenGetterCode}`
+      );
+      if (!getSessionTokenResp.ok) {
+        throw new Error(
+          `Failed to get session token (HTTP ${getSessionTokenResp.status})`
+        );
+      }
+
+      const sessionTokenResp = await getSessionTokenResp.text();
+      const { sessionToken } = JSON.parse(sessionTokenResp);
 
       await setSessionToken(sessionToken);
       router.replace("/main");
@@ -62,6 +76,8 @@ export default function Index() {
       if (error.message !== "cancel") {
         Alert.alert(t("common.error"), error.message);
       }
+    } finally {
+      setHandleLoginInProgress(false);
     }
   };
 
@@ -79,6 +95,7 @@ export default function Index() {
       <Button
         title={t("common.login")}
         disabled={!request}
+        loading={handleLoginInProgress}
         onPress={handleLogin}
         buttonStyle={styles.button}
         titleStyle={styles.buttonText}
