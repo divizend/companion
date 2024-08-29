e2139c720f11a58ca45c2db9ef96cb48d762c367
Julian Nalenz <julian.nalenz@divizend.com>
2024-08-29 04:52:54 +0200

    feat: add typing for config

--- app/common/config.tsx
+++ app/common/config.tsx
@@ -1,3 +1,15 @@
 import Constants from "expo-constants";
 
-export const usedConfig = Constants.expoConfig!.extra;
+export type Config = {
+  auth: {
+    url: string;
+    clientId: string;
+    tenantId: string;
+  };
+  api: {
+    url: string;
+    versionCode: string;
+  };
+};
+
+export const usedConfig = Constants.expoConfig!.extra! as Config;
