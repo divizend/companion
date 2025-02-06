b1d9273568db12cc3c3f652cb1f7be1c7c0b65de
Feriel Bouhamed <bouhamedfarioula@gmail.com>
2025-02-07 00:42:29 +0100

    feat: enhance portfolio connection functionality with new import session and progress tracking

--- common/portfolioConnect.ts
+++ common/portfolioConnect.ts
@@ -1,49 +1,132 @@
 import { portfolioConnect } from '@/signals/portfolioConnect';
+import {
+  createDepotImportSessionSuccess,
+  finalizeImport,
+  getSecapiImportProgressFailure,
+  getSecapiImportProgressLoading,
+  getSecapiImportProgressSuccess,
+  portfolioContentsImportDepotsSetLoading,
+  resetPortfolioConnect,
+  setSecapiImportProgressRequestTimestamp,
+  startSecapiImportFailure,
+  startSecapiImportSuccess,
+} from '@/signals/portfolioConnectActions';
 
-import { usePost } from './api';
+import { apiGet, apiPatch, apiPostJson } from './api';
 
-export function portfolioContentsImportConnectDepots(multiAccountImport: any) {
-  const chosenAccountIds = portfolioConnect.value.importDepots?.chosenDepotIds;
+export async function portfolioContentsImportConnectDepots({
+  multiAccountImport,
+  ownerEntityId,
+}: {
+  multiAccountImport?: string;
+  ownerEntityId: string;
+}) {
+  const chosenAccountIds = portfolioConnect.value.importDepots.chosenDepotIds;
+
   const accounts = JSON.parse(
     JSON.stringify(
-      portfolioConnect.value.portfolioContents?.accounts.filter(acc => chosenAccountIds?.includes(acc.id)),
+      portfolioConnect.value.portfolioContents.accounts!.filter(acc => chosenAccountIds?.includes(acc.id)),
     ),
   );
 
-  const secapiImportId = portfolioConnect.value.secapiImport?.id;
+  const secapiImportId = portfolioConnect.value.secapiImport.id;
+
   const payload = {
-    ...(secapiImportId ? { secapiImportId } : {}), // not needed for manual imports
+    ...(secapiImportId ? { secapiImportId } : {}),
     bankInfo: portfolioConnect.value.bank,
-    ownerEntityId: '',
-    accounts: accounts.map((acc: any) => ({
-      ...acc,
-      depotNumber: portfolioConnect.value.depotNumberToSync ?? acc.depotNumber,
-      newDepotNumber: acc.depotNumber,
-    })),
+    ownerEntityId,
+    accounts: accounts.map((acc: any) => {
+      return {
+        ...acc,
+        depotNumber: portfolioConnect.value.depotNumberToSync ?? acc.depotNumber,
+        newDepotNumber: acc.depotNumber,
+      };
+    }),
     unassignedOrganization: multiAccountImport,
   };
 
-  const mutation = usePost('importDepots', '/depots/import');
+  portfolioContentsImportDepotsSetLoading(true);
 
-  return {
-    importDepots: () => {
-      mutation.mutate(payload, {
-        onSuccess: data => {
-          const securities = portfolioConnect.value.portfolioContents?.accounts?.[0]?.securities;
-          // finalizeImport({
-          //   done: portfolioConnectDoneSelector(),
-          //   empty: !securities || Object.keys(securities).length === 0,
-          //   data,
-          // });
-        },
-        onError: error => {
-          // snackbarError('portfolioConnect:portfolioContents.importError', { error });
-          // reset();
-        },
+  return apiPostJson('/depots/import', payload)
+    .then((data: any) => {
+      const securities = portfolioConnect.value.portfolioContents?.accounts?.[0]?.securities;
+      finalizeImport({
+        done: true,
+        empty: !securities || Object.keys(securities).length === 0,
+        data,
       });
-    },
-    isLoading: mutation.isPending,
-    isError: mutation.isError,
-    error: mutation.error,
-  };
+    })
+    .catch((error: any) => {
+      // use snackbar later
+      console.error('portfolioContentsImportConnectDepots', error);
+      resetPortfolioConnect();
+    })
+    .finally(() => {
+      portfolioContentsImportDepotsSetLoading(false);
+    });
 }
+
+export function sendDepotImportEvent(sessionId: string, data: any) {
+  return apiPostJson(`/depot-import/${sessionId}/event`, data);
+}
+
+export const createDepotImportSession = async ({ depotNumberToSync }: { depotNumberToSync?: string } = {}) => {
+  return apiPostJson('/depot-import/session', { depotNumberToSync }).then((sessionId: string) => {
+    createDepotImportSessionSuccess(sessionId);
+  });
+};
+
+export const setDepotImportSessionReplayId = (sessionId: string, replayId: string) => {
+  return apiPatch(`/depot-import/${sessionId}/replay/${replayId}`, {});
+};
+
+export const getSecapiImportProgress = () => {
+  const portfolioConnectValue = portfolioConnect.value;
+  if (!portfolioConnectValue.secapiImport.id) return; // an import id must exist
+  if (portfolioConnectValue.portfolioContents?.keys) return; // do not poll if portfolio contents are not empty
+  if (portfolioConnectValue.secapiImport.loading) return; // do not poll if an ongoing request has not yet resolved
+
+  const requestTimestamp = +new Date();
+  setSecapiImportProgressRequestTimestamp(requestTimestamp);
+
+  getSecapiImportProgressLoading(true);
+
+  return apiGet('/secapiImport', { secapiImportId: portfolioConnectValue.secapiImport.id })
+    .then(res => {
+      getSecapiImportProgressSuccess({ res, requestTimestamp });
+    })
+    .catch(error => {
+      getSecapiImportProgressFailure({ error, requestTimestamp });
+    })
+    .finally(() => {
+      getSecapiImportProgressLoading(false);
+    });
+};
+
+export const watchImportProgress = async (depotNumberToSync?: string) => {
+  const portfolioConnectValue = portfolioConnect.value;
+  if (portfolioConnectValue.portfolioContents?.keys) return; // do not load accounts if portfolio contents are not empty
+  if (portfolioConnectValue.secapiImport?.id) return; // an import id already exists
+
+  const message = portfolioConnectValue.secapiImport?.successMessage!;
+  // This timeout is for avoiding creating 2 entities since the secapi import is also started from the backend
+  await new Promise(r => setTimeout(r, 1000));
+  const payload = {
+    token: message.token,
+    interfaceType: message.interface.type,
+    interfaceId: message.interface.id,
+    bankId: message.bank.id,
+    flags: message.flags,
+    depotNumberToSync,
+  };
+  return apiPostJson('/secapiImport', payload)
+    .then(res => {
+      startSecapiImportSuccess({
+        ...res,
+        depotNumberToSync,
+      });
+    })
+    .catch(error => {
+      startSecapiImportFailure(error);
+    });
+};
--- common/profile.ts
+++ common/profile.ts
@@ -49,6 +49,7 @@ export type CompanionProfile = {
 export type LegalEntity = {
   id: string;
   isPrincipal: boolean;
+  isActive: boolean;
   data: {
     info: {
       givenName: string;
@@ -69,6 +70,8 @@ export type UserProfile = {
     allowedCompanionAI?: boolean;
     usedCompanionTrial?: boolean;
     canAccessCompanionFeaturesWithoutSubscription?: boolean;
+    canAccessDemoBanks?: boolean;
+    useLocalSecapi?: boolean;
   };
   impersonation?: {
     fromEmail: string;
