35854d05239f04c503cdbbd42c6938c0211c4b08
Alaa Cherif <alaac617@gmail.com>
2025-03-17 16:38:52 +0100

    fix: remove unmatched route error on login android

---
+++ app/authcallback.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import FullScreenActivityIndicator from '@/components/FullScreenActivityIndicator';
+
+export default function AuthCallBack() {
+  return <FullScreenActivityIndicator />;
+}
