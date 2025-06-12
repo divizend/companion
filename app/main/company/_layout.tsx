d3906ba3d9ff4ab37231ba3a1a462b07e834bb91
Mohamed Aziz Khayati <mohamedazizkhayati@gmail.com>
2025-06-12 11:15:11 +0100

    fix: use safeareaview for IOS aswell

--- app/main/company/_layout.tsx
+++ app/main/company/_layout.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
 import { Slot } from 'expo-router';
-import { Platform, View } from 'react-native';
 
 import { SafeAreaView } from '@/components/base';
 
 export default function Layout() {
-  const ParentView = Platform.OS === 'ios' ? View : SafeAreaView;
-
   return (
-    <ParentView className="flex-1 dark:bg-primary-dark bg-primary-light">
+    <SafeAreaView className="flex-1 dark:bg-primary-dark bg-primary-light">
       <Slot />
-    </ParentView>
+    </SafeAreaView>
   );
 }
