9edb3447d4399e125b0672a389186b0fd95d2794
Alaa Cherif <alaac617@gmail.com>
2025-07-15 08:23:30 +0100

    fix: make table not scrollable and full width

--- components/features/actor/CompanyDividendTableWidget.tsx
+++ components/features/actor/CompanyDividendTableWidget.tsx
@@ -84,73 +84,67 @@ export default function CompanyDividendTableWidget({ security }: { security: Sec
 
   return (
     <Widget title={t('actor.dividendTableWidget.title')} ready={!isLoading}>
-      <ScrollView horizontal>
-        <View className="bg-primary-light dark:bg-primary-dark rounded-lg overflow-hidden min-w-[350]">
-          <View className="flex-row px-3 py-2">
-            <Text className="uppercase flex-1 font-bold text-xs">{t('actor.dividendTableWidget.exDate')}</Text>
-            <Text className="uppercase flex-1 font-bold text-xs">{t('actor.dividendTableWidget.yoy')}</Text>
-            <Text className="uppercase flex-1 font-bold text-xs text-right">
-              {t('actor.dividendTableWidget.amount')}
-            </Text>
-          </View>
-          {Object.keys(grouped)
-            .sort((a, b) => Number(b) - Number(a))
-            .map(year => (
-              <View key={year}>
-                <Pressable onPress={() => toggleYear(year)}>
-                  <View className="flex-row bg-secondary-light dark:bg-secondary-dark px-3 py-2 items-center">
-                    <Text className="flex-1 font-bold">{year}</Text>
-                    <View
-                      className={`px-2 py-0.5 rounded-md inline-flex items-center ${
-                        !!yoyGrowth[year]
-                          ? yoyGrowth[year] < 0
-                            ? 'bg-red-100 dark:bg-red-900'
-                            : 'bg-green-100 dark:bg-green-900'
-                          : ''
-                      }`}
-                    >
-                      {yoyGrowth[year] ? (
-                        <Text
-                          className={`font-bold ${
-                            yoyGrowth[year] < 0
-                              ? 'text-red-700 dark:text-red-300'
-                              : 'text-green-700 dark:text-green-300'
-                          }`}
-                        >
-                          {yoyGrowth[year] < 0 ? '↓' : '↑'}{' '}
-                          {t('percent', {
-                            value: {
-                              value: Math.abs(yoyGrowth[year]),
-                              minimumFractionDigits: 2,
-                              maximumFractionDigits: 2,
-                            },
-                          })}
-                        </Text>
-                      ) : (
-                        ''
-                      )}
-                    </View>
-                    <Text className="flex-1 font-bold text-right">
-                      {t('currency', {
-                        amount: { amount: totalDividendsPerYear[year], unit: data?.dividends?.[0].yield.unit && 'EUR' },
-                      })}
+      <View className="bg-primary-light dark:bg-primary-dark rounded-lg">
+        <View className="flex-row px-3 py-2">
+          <Text className="uppercase flex-1 font-bold text-xs">{t('actor.dividendTableWidget.exDate')}</Text>
+          <Text className="uppercase flex-1 font-bold text-xs">{t('actor.dividendTableWidget.yoy')}</Text>
+          <Text className="uppercase flex-1 font-bold text-xs text-right">{t('actor.dividendTableWidget.amount')}</Text>
+        </View>
+        {Object.keys(grouped)
+          .sort((a, b) => Number(b) - Number(a))
+          .map(year => (
+            <View key={year}>
+              <Pressable onPress={() => toggleYear(year)}>
+                <View className="flex-row bg-secondary-light dark:bg-secondary-dark px-3 py-2 items-center">
+                  <Text className="flex-1 font-bold">{year}</Text>
+                  <View
+                    className={`px-2 py-0.5 rounded-md inline-flex items-center ${
+                      !!yoyGrowth[year]
+                        ? yoyGrowth[year] < 0
+                          ? 'bg-red-100 dark:bg-red-900'
+                          : 'bg-green-100 dark:bg-green-900'
+                        : ''
+                    }`}
+                  >
+                    {yoyGrowth[year] ? (
+                      <Text
+                        className={`font-bold ${
+                          yoyGrowth[year] < 0 ? 'text-red-700 dark:text-red-300' : 'text-green-700 dark:text-green-300'
+                        }`}
+                      >
+                        {yoyGrowth[year] < 0 ? '↓' : '↑'}{' '}
+                        {t('percent', {
+                          value: {
+                            value: Math.abs(yoyGrowth[year]),
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          },
+                        })}
+                      </Text>
+                    ) : (
+                      ''
+                    )}
+                  </View>
+                  <Text className="flex-1 font-bold text-right">
+                    {t('currency', {
+                      amount: { amount: totalDividendsPerYear[year], unit: data?.dividends?.[0].yield.unit && 'EUR' },
+                    })}
+                  </Text>
+                </View>
+              </Pressable>
+              {expandedYears[year] &&
+                (grouped[year] as any[]).map((div: any, idx: number) => (
+                  <View key={div.date + idx} className="flex-row px-3 py-2">
+                    <Text className="flex-1 text-xs">{t('dateTime.day', { date: new Date(div.date) })}</Text>
+                    <Text className="flex-1 text-xs"></Text>
+                    <Text className="flex-1 text-xs text-right">
+                      {t('currency', { amount: { amount: div.yield.amount, unit: div.yield.unit || 'EUR' } })}
                     </Text>
                   </View>
-                </Pressable>
-                {expandedYears[year] &&
-                  (grouped[year] as any[]).map((div: any, idx: number) => (
-                    <View key={div.date + idx} className="flex-row px-3 py-2">
-                      <Text className="flex-1 text-xs">{t('dateTime.day', { date: new Date(div.date) })}</Text>
-                      <Text className="flex-1 text-xs"></Text>
-                      <Text className="flex-1 text-xs text-right">
-                        {t('currency', { amount: { amount: div.yield.amount, unit: div.yield.unit || 'EUR' } })}
-                      </Text>
-                    </View>
-                  ))}
-              </View>
-            ))}
-        </View>
-      </ScrollView>
+                ))}
+            </View>
+          ))}
+      </View>
     </Widget>
   );
 }
